dc29359b180e7fbab8799ef39d5c7b84ef0c0ecf
rgrr <ntbox@gmx.net>
2025-02-11 07:15:04 +0100

    minor

--- testing/justdump.py
+++ testing/justdump.py
@@ -2,7 +2,6 @@ import serial
 from datetime import datetime
 import json
 
-nowstamp = datetime.now().astimezone().isoformat(timespec="seconds")
 with (
     serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.001, bytesize=serial.EIGHTBITS,
                   stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_SPACE, 
@@ -13,7 +12,7 @@ with (
         data = s.read(100)
         if len(data) != 0:
             now_t = datetime.now()
-            line = {"time": datetime.now().astimezone().isoformat(), 
+            line = {"time": now_t.astimezone().isoformat(), 
                     "dt_ms": (now_t - prev_t).microseconds / 1000.0, 
                     "data": data.hex()}
             print(line)
