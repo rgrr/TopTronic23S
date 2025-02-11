89ea02cd54092f55489cec895eca7eef7794eb14
rgrr <ntbox@gmx.net>
2025-02-11 21:01:26 +0100

    ...

--- testing/justpackets.py
+++ testing/justpackets.py
@@ -17,6 +17,7 @@ g_if = "/dev/ttyUSB0"
 g_if_parity = serial.PARITY_SPACE     # Note: there seems to be some kind of parity on the line.  Not sure which one though
 g_mqtt_server = "127.0.0.1:1883"
 g_mqtt_topic = "ds"
+g_show_unknown = False
 
 packet_state = "wait-start"
 packet = bytearray()
@@ -110,9 +111,10 @@ with (
                         
                         if packet_cmd == 4:
                             #
-                            # currect state
+                            # current state
                             #
                             try:
+                                # outised temperature
                                 t = packet[5] / 2.0 - 52.0
                                 line["temp-out"] = t
                                 if mqttc is not None:
@@ -120,6 +122,7 @@ with (
                                 if g_mask:
                                     data = "__" + data[2:]
                                 
+                                # unknown temperature
                                 t = packet[20] / 2.0
                                 line["tempx"] = t
                                 if mqttc is not None:
@@ -127,19 +130,33 @@ with (
                                 if g_mask:
                                     data = data[:30] + "__" + data[32:]
 
+                                # kessel(?) temperature
                                 t = packet[35] / 2.0
                                 line["temp-kessel"] = t
                                 if mqttc is not None:
                                     mqttc.publish("%s/%s" % (g_mqtt_topic, "temp-kessel"), t, qos=0)
                                 if g_mask:
                                     data = data[:60] + "__" + data[62:]
+                                
+                                if not g_show_unknown:
+                                    # unknown: values seen are 0x00, 0x02, 0xff
+                                    if g_mask:
+                                        data = data[0:4] + ".." + data[6:]
+
+                                    # unknown: values seen are 0x00, 0x10
+                                    if g_mask:
+                                        data = data[0:6] + ".." + data[8:]
+
+                                    # unknown: values seen are 0x6a, 0xa6
+                                    if g_mask:
+                                        data = data[0:12] + ".." + data[14:]
 
                                 line["data"] = data
                                 if g_mask  and  data != data_prev:
                                     line["changed"] = 1
+                                    if data_prev != ""  and  mqttc is not None:
+                                        mqttc.publish("%s/%s" % (g_mqtt_topic, "cmd4-changed"), str(line), qos=0)
                                     data_prev = data
-                                    if mqttc is not None:
-                                        mqttc.publish("%s/%s" % (g_mqtt_topic, "cmd4-changed"), data, qos=0)
 
                                 print(line)
                                 prev_t = now_t
